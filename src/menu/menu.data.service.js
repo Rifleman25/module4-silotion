f6743685d96fb9e056974782dbdc2edf010f42ec
rifleman25 <aist2596@mail.ru>
2019-09-01 20:19:20 +0500

    5

--- src/menu/menu.data.service.js
+++ src/menu/menu.data.service.js
@@ -7,7 +7,7 @@ angular.module('Data')
 
 
 MenuDataService.$inject = ['$http', 'ApiBasePath']
-function MenuDataService($q, $timeout) {
+function MenuDataService($http, ApiBasePath) {
   var service = this;
 
   service.getAllCategories = function () {
